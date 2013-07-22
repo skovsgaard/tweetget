0db37d56bb798165eeaa6f28e3e390099d7d107f
jschrist <bastian.1343@gmail.com>
2013-07-22 15:00:32 -0400

    Updated Tweetget to stream to file

--- tweetget.js
+++ tweetget.js
@@ -1,19 +1,22 @@
-var request = require('request'),
-    cheerio = require('cheerio'),
-    target = 'http://www.twitter.com/' + process.argv[2];
+var request = require('request');
+var cheerio = require('cheerio');
+var fs      = require('fs');
+var target  = 'http://www.twitter.com/' + process.argv[2];
 
 request(target, function(err, res, body) {
   if (!err && res.statusCode == 200) {
+    var tweets = [];
+    var filer = fs.createWriteStream('tweets.txt');
     var $ = cheerio.load(body, {
       ignoreWhitespace: true,
       lowerCaseTags: true
     });
-    var tweets = [];
     $('.tweet-text').each(function(i, element) {
       tweets[i] = $(this).text();
     });
     tweets.forEach(function(i) {
-      console.log(i + "\n");
-    })
+      console.log(i + '\n');
+      filer.write(i + '\n --- \n');
+    });
   }
 });
