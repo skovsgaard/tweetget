cd79a822f7e211e988846ca043b7c2080c7d0429
jschrist <bastian.1343@gmail.com>
2013-07-25 22:38:16 -0400

    Updated tweetget to take an optional third argument, -w, for writing to tweets.txt.

--- tweetget.js
+++ tweetget.js
@@ -2,11 +2,14 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs      = require('fs');
 var target  = 'http://www.twitter.com/' + process.argv[2];
+var flag    = process.argv[3];
 
 request(target, function(err, res, body) {
   if (!err && res.statusCode == 200) {
     var tweets = [];
-    var filer = fs.createWriteStream('tweets.txt');
+    if (flag == '-w') {
+      var filer = fs.createWriteStream('tweets.txt');
+    }
     var $ = cheerio.load(body, {
       ignoreWhitespace: true,
       lowerCaseTags: true
@@ -15,8 +18,11 @@ request(target, function(err, res, body) {
       tweets[i] = $(this).text();
     });
     tweets.forEach(function(i) {
-      console.log(i + '\n');
-      filer.write(i + '\n --- \n');
+      if (!flag) {
+        console.log(i + '\n ---');
+      } else if (flag == '-w') {
+        filer.write(i + '\n --- \n');
+      }
     });
   }
 });
