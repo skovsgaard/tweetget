6f7bbf83210ac7180917417434979d0e68ced918
jschrist <bastian.1343@gmail.com>
2013-07-30 00:04:33 -0400

    tweetget() no longer console.logs by default)

--- tweetget.js
+++ tweetget.js
@@ -3,22 +3,19 @@ var cheerio = require('cheerio');
 var fs      = require('fs');
 
 function tweetget(handle, cb) {
+  var tweets = [];
   var target = "http://twitter.com/" + handle;
   request(target, function(err, res, body) {
     if (!err && res.statusCode == 200) {
-      var tweets = [];     
       var $ = cheerio.load(body, {
         ignoreWhitespace: true,
         lowerCaseTags: true
       });      
       $('.tweet-text').each(function(i, element) {
         tweets[i] = $(this).text();
-      });      
-      tweets.forEach(function(i) {
-        //console.log(i + '\n ---');
-        return i + '\n --- \n';
       });
       cb(tweets);
+      return tweets;
     }
   });
 }
